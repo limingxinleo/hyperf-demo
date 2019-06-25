97913f26d3fceb99243035ebf57f67ee0f81a639
李铭昕 <715557344@qq.com>
2019-06-25 10:50:51 +0800

    Added db test.

--- app/Controller/DbController.php
+++ app/Controller/DbController.php
@@ -1,9 +1,18 @@
 <?php
 
 declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://doc.hyperf.io
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
+ */
 
 namespace App\Controller;
 
+use App\Service\UserService;
 use Hyperf\HttpServer\Annotation\AutoController;
 use Hyperf\HttpServer\Contract\RequestInterface;
 use Hyperf\HttpServer\Contract\ResponseInterface;
@@ -15,6 +24,6 @@ class DbController
 {
     public function index(RequestInterface $request, ResponseInterface $response)
     {
-        return $response->raw('Hello Hyperf!');
+        return di()->get(UserService::class)->find(1);
     }
 }
---
+++ app/Service/UserService.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://doc.hyperf.io
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
+ */
+
+namespace App\Service;
+
+use App\Model\User;
+use Hyperf\Cache\Annotation\Cacheable;
+
+class UserService
+{
+    /**
+     * @Cacheable(prefix="cache:user", value="#{userId}")
+     */
+    public function find(int $userId)
+    {
+        return User::query()->find($userId)->toArray();
+    }
+}
