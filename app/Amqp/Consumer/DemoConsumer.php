4b7efe54008e6e6f16b12d4f742dcaabfee70873
李铭昕 <715557344@qq.com>
2019-07-24 17:38:02 +0800

    Update

---
+++ app/Amqp/Consumer/DemoConsumer.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://doc.hyperf.io
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
+ */
+
+namespace App\Amqp\Consumer;
+
+use Hyperf\Amqp\Annotation\Consumer;
+use Hyperf\Amqp\Message\ConsumerMessage;
+use Hyperf\Amqp\Result;
+
+/**
+ * @Consumer(exchange="hyperf", routingKey="hyperf", queue="hyperf", nums=1)
+ */
+class DemoConsumer extends ConsumerMessage
+{
+    public function consume($data): string
+    {
+        var_dump($data);
+        return Result::ACK;
+    }
+}
---
+++ app/Controller/AmqpController.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://doc.hyperf.io
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
+ */
+
+namespace App\Controller;
+
+use App\Amqp\Producer\DemoProducer;
+use Hyperf\HttpServer\Annotation\AutoController;
+
+/**
+ * @AutoController
+ */
+class AmqpController extends Controller
+{
+    public function index()
+    {
+        $res = amqp_produce(new DemoProducer(uniqid()));
+
+        return $this->response->success($res);
+    }
+}
