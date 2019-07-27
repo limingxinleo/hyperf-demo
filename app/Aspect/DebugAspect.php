42f77bead8c90b19248803ab6e4fe38b32dfd2ed
李铭昕 <715557344@qq.com>
2019-07-27 08:58:34 +0800

    Update

---
+++ app/Aspect/DebugAspect.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://doc.hyperf.io
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
+ */
+
+namespace App\Aspect;
+
+use App\Service\AspectService;
+use Hyperf\Di\Annotation\Aspect;
+use Hyperf\Di\Aop\AbstractAspect;
+use Hyperf\Di\Aop\ProceedingJoinPoint;
+use Psr\Container\ContainerInterface;
+
+/**
+ * @Aspect
+ */
+class DebugAspect extends AbstractAspect
+{
+    public $classes = [
+        AspectService::class . '::test*',
+        'App\Service\AspectClass*',
+        AspectService::class . '::handle',
+    ];
+
+    protected $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    public function process(ProceedingJoinPoint $proceedingJoinPoint)
+    {
+        return $proceedingJoinPoint->process();
+    }
+}
---
+++ app/Service/AspectClass2Service.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://doc.hyperf.io
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
+ */
+
+namespace App\Service;
+
+class AspectClass2Service
+{
+    public function test()
+    {
+    }
+
+    public function test2()
+    {
+    }
+
+    public function handle()
+    {
+    }
+
+    public function handle2()
+    {
+    }
+}
