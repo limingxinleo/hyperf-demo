aec835a229a179eaa08a7b74152d98d68f761590
李铭昕 <715557344@qq.com>
2019-08-15 18:14:04 +0800

    Update

--- app/Controller/QueueController.php
+++ app/Controller/QueueController.php
@@ -48,4 +48,35 @@ class QueueController extends Controller
 
         return 'success';
     }
+
+    public function annotation()
+    {
+        di()->get(QueueService::class)->annotation([
+            'group@hyperf.io',
+            'https://doc.hyperf.io',
+            'https://www.hyperf.io',
+        ]);
+
+        return 'success';
+    }
+
+    public function annotation2()
+    {
+        di()->get(QueueService::class)->annotationdelay([
+            'group@hyperf.io',
+            'https://doc.hyperf.io',
+            'https://www.hyperf.io',
+        ]);
+
+        return 'success';
+    }
+
+    public function annotation3()
+    {
+        $user = User::query()->find(1);
+
+        di()->get(QueueService::class)->annotationmodel($user);
+
+        return 'success';
+    }
 }
--- app/Service/QueueService.php
+++ app/Service/QueueService.php
@@ -13,6 +13,8 @@ declare(strict_types=1);
 namespace App\Service;
 
 use App\Job\ExampleJob;
+use App\Model\User;
+use Hyperf\AsyncQueue\Annotation\AsyncQueueMessage;
 use Hyperf\AsyncQueue\Driver\DriverFactory;
 use Hyperf\AsyncQueue\Driver\DriverInterface;
 
@@ -39,5 +41,33 @@ class QueueService
         // 这里的 `ExampleJob` 会被序列化存到 Redis 中，所以内部变量最好只传入普通数据
         // 同理，如果内部使用了注解 @Value 会把对应对象一起序列化，导致消息体变大。
         return $this->driver->push(new ExampleJob($params), $delay);
+    }
+
+    /**
+     * @AsyncQueueMessage
+     * @param mixed $params
+     */
+    public function annotation($params)
+    {
+        var_dump($params);
+    }
+
+    /**
+     * @AsyncQueueMessage(delay=1)
+     * @param mixed $params
+     */
+    public function annotationdelay($params)
+    {
+        var_dump($params);
+    }
+
+    /**
+     * @AsyncQueueMessage(delay=1)
+     * @param mixed $params
+     */
+    public function annotationmodel(User $user)
+    {
+        var_dump(111);
+        var_dump($user);
     }
 }
