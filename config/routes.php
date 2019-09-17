d11c03ee805278ebc9f009c1a0f11e431ad62cd4
李铭昕 <715557344@qq.com>
2019-09-17 18:55:30 +0800

    Update

--- config/routes.php
+++ config/routes.php
@@ -13,3 +13,4 @@ declare(strict_types=1);
 use Hyperf\HttpServer\Router\Router;
 
 Router::addRoute(['GET', 'POST', 'HEAD'], '/', 'App\Controller\IndexController@index');
+Router::addRoute(['GET', 'POST', 'HEAD'], '/index.json', 'App\Controller\IndexController@index');
--- storage/view/index.blade.php
+++ storage/view/index.blade.php
@@ -7,5 +7,13 @@
 <body>
 Hello, {{$name}}. You are using blade template now.
 </body>
+<script src="https://code.jquery.com/jquery-2.2.4.min.js"
+        integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
+        crossorigin="anonymous"></script>
 <script src="/debug.js"></script>
+<script>
+    $.post('/index.json', function (res) {
+        console.log(res);
+    });
+</script>
 </html>
