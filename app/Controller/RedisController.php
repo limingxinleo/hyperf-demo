70a87cc0d6086bb764dd3920c3f12065750aee97
李铭昕 <715557344@qq.com>
2019-10-28 11:52:39 +0800

    Create RedisController.php

---
+++ app/Controller/RedisController.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://doc.hyperf.io
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+
+namespace App\Controller;
+
+use Hyperf\HttpServer\Annotation\AutoController;
+
+/**
+ * @AutoController
+ */
+class RedisController extends Controller
+{
+    public function pipeline()
+    {
+        $redis = di()->get(\Redis::class);
+
+        for ($i = 0; $i < 100; ++$i) {
+            $pipe = $redis->multi(\Redis::PIPELINE);
+            $pipe->set('xxx', uniqid());
+            $pipe->set('xxx', uniqid());
+            $pipe->exec();
+        }
+
+        return $this->response->success('Hello Hyperf!');
+    }
+}
