ae69f51315fe2e5a9533743d083f0588ed6aa246
李铭昕 <715557344@qq.com>
2019-10-29 10:43:28 +0800

    Update dependencies.php

--- config/autoload/dependencies.php
+++ config/autoload/dependencies.php
@@ -12,4 +12,5 @@ declare(strict_types=1);
 
 return [
     // Hyperf\Contract\StdoutLoggerInterface::class => App\Kernel\Log\LoggerFactory::class,
+    'WebSocketServer' => Hyperf\WebSocketServer\Server::class,
 ];
