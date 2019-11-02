e02451a1b165982e8aeb352076846c580ae82145
李铭昕 <715557344@qq.com>
2019-11-02 13:17:41 +0800

    Update

---
+++ app/Crontab/DemoCrontab.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://doc.hyperf.io
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+
+namespace App\Crontab;
+
+use Hyperf\Crontab\Annotation\Crontab;
+
+/**
+ * @Crontab(name="RefreshRepoStarCount", rule="* * * * *", callback="execute", singleton=true)
+ */
+class DemoCrontab
+{
+    public function execute()
+    {
+        var_dump('before crontab.');
+        sleep(1);
+        var_dump('after crontab.');
+    }
+}
