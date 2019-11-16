2105888165c0cb45d7c762c25eb1a8be57576fda
李铭昕 <715557344@qq.com>
2019-11-16 19:20:19 +0800

    Update DemoCrontab.php

--- app/Crontab/DemoCrontab.php
+++ app/Crontab/DemoCrontab.php
@@ -12,18 +12,26 @@ declare(strict_types=1);
 
 namespace App\Crontab;
 
+use Hyperf\Contract\StdoutLoggerInterface;
 use Hyperf\Crontab\Annotation\Crontab;
+use Hyperf\Di\Annotation\Inject;
 
 /**
  * @Crontab(name="RefreshRepoStarCount", rule="* * * * * *", callback="execute", singleton=true)
  */
 class DemoCrontab
 {
+    /**
+     * @Inject()
+     * @var StdoutLoggerInterface
+     */
+    protected $logger;
+
     public function execute()
     {
         $a = '123';
         var_dump('before crontab.');
-        var_dump(date('Y-m-d H:i:s', (int) $a));
+        $this->logger->info(date('Y-m-d H:i:s', (int)$a));
         var_dump('after crontab.');
     }
 }
