6108bddb2548dbaa8e631b00d28fe0810b9d3484
李铭昕 <715557344@qq.com>
2020-01-10 21:31:37 +0800

    Update

--- app/Amqp/Consumer/TestConsumer.php
+++ app/Amqp/Consumer/TestConsumer.php
@@ -1,15 +1,23 @@
 <?php
 
 declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://doc.hyperf.io
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
 
 namespace App\Amqp\Consumer;
 
-use Hyperf\Amqp\Result;
 use Hyperf\Amqp\Annotation\Consumer;
 use Hyperf\Amqp\Message\ConsumerMessage;
+use Hyperf\Amqp\Result;
 
 /**
- * @Consumer(exchange="hyperf", routingKey="hyperf", queue="hyperf", name ="TestConsumer", nums=1, enable=false)
+ * @Consumer(exchange="hyperf", routingKey="hyperf", queue="hyperf", name="TestConsumer", nums=1)
  */
 class TestConsumer extends ConsumerMessage
 {
--- app/Amqp/Producer/TestProducer.php
+++ app/Amqp/Producer/TestProducer.php
@@ -1,10 +1,17 @@
 <?php
 
 declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://doc.hyperf.io
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
 
 namespace App\Amqp\Producer;
 
-use Hyperf\Amqp\Annotation\Producer;
 use Hyperf\Amqp\Message\ProducerMessage;
 
 class TestProducer extends ProducerMessage
