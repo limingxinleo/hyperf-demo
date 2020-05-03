9486c2fa13097978c970b102afeae1e73ca837d0
李铭昕 <715557344@qq.com>
2020-05-03 20:06:14 +0800

    Update

---
+++ app/Controller/WebSocketController.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://doc.hyperf.io
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+namespace App\Controller;
+
+use Hyperf\SocketIOServer\Annotation\Event;
+use Hyperf\SocketIOServer\Annotation\SocketIONamespace;
+use Hyperf\SocketIOServer\BaseNamespace;
+use Hyperf\SocketIOServer\Socket;
+use Hyperf\Utils\Codec\Json;
+
+/**
+ * @SocketIONamespace("/")
+ */
+class WebSocketController extends BaseNamespace
+{
+    /**
+     * @Event("event")
+     * @param string $data
+     */
+    public function onEvent(Socket $socket, $data)
+    {
+        return 'Event Received: ' . $data;
+    }
+
+    /**
+     * @Event("join-room")
+     * @param string $data
+     */
+    public function onJoinRoom(Socket $socket, $data)
+    {
+        $socket->join($data);
+        $socket->to($data)->emit('event', $socket->getSid() . "has joined {$data}");
+        $socket->emit('event', 'There are ' . count($socket->getAdapter()->clients($data)) . " players in {$data}");
+    }
+
+    /**
+     * @Event("say")
+     * @param string $data
+     */
+    public function onSay(Socket $socket, $data)
+    {
+        $data = Json::decode($data);
+        $socket->to($data['room'])->emit('event', $socket->getSid() . " say: {$data['message']}");
+    }
+}
